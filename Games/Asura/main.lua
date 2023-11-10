81e2c0fa5d098efe9497f72156dca795f7448068
Gouang <131418880+i7hz@users.noreply.github.com>
2023-11-10 17:13:40 +0700

    Add UI (Orion) & re-write farm

--- Games/Asura/main.lua
+++ Games/Asura/main.lua
@@ -1,8 +1,13 @@
 repeat task.wait() until game:IsLoaded()
 
---/ Auto Task/Job
 local Players = game:GetService("Players")
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Client = Players.LocalPlayer
+local PlayerGui = Client.PlayerGui;
+local Configs = {
+    farmSpeed = 12
+}
 
 function Tp(coordinate: CFrame, speed: number)
     local Client = Players.LocalPlayer
@@ -20,91 +25,209 @@ function Tp(coordinate: CFrame, speed: number)
     end
 end
 
-getgenv().JobFarm = true;
-local farmSpeed = 12
+function GetJob()
+    local Remote = ReplicatedStorage.Events.EventCore;
+    local MainGui = PlayerGui.MainGui
+    local jobText, cancelJob = MainGui.LabelJob.Text, MainGui.CancelJob
 
-while getgenv().JobFarm do
-    local Client = Players.LocalPlayer;
-    local Character = Client.Character or Client.CharacterAdded:Wait();
-
-    if Character then
-        local Events = ReplicatedStorage.Events
-        local CoreEvent = Events.EventCore
-        local MainGui = Client.PlayerGui:FindFirstChild("Main")
-
-        local firedArguments = {[1] = "Job"}
-        local Job = MainGui.LabelJob.Text
-        local TimeJob = MainGui.LabelJob2.Text
-        local Paycheck = MainGui.LabelJob3.Text
-        local CancelJob = MainGui.CancelJob
-
-        if not CancelJob.Visible then
-            repeat task.wait()
-                CoreEvent:FireServer(unpack(firedArguments))
-            until CancelJob.Visible and Client.PlayerGui:FindFirstChildWhichIsA("BillboardGui")
-        end
-
+    if not cancelJob.Visible and #jobText <= 1 then
+        Remote:FireServer("Job")
         task.wait(1)
+    end
 
-        local Billboard
-        repeat
-            Billboard = Client.PlayerGui:FindFirstChild("BillboardGui") or Client.PlayerGui:FindFirstChildOfClass("BillboardGui")
-            task.wait()
-        until Billboard
+    return cancelJob.Visible;
+end
 
-        if #Job > 0 and CancelJob.Visible and Billboard.Enabled then
-            local TargetPart = Billboard.Adornee
-            local PrimaryPart = Character:FindFirstChild("HumanoidRootPart")
+function GetLocation()
+    local obj = PlayerGui:FindFirstChildOfClass("BillboardGui") or nil;
+    return obj
+end
 
-            --: Cleaning
-            if tostring(TargetPart):match("Entrance") then
-                if CLient:DistanceFromCharacter(TargetPart.Position) > 40 then
-                    local ModifyCFrame = PrimaryPart.CFrame + Vector3.new(0, -20, 0)
-                    Tp(ModifyCFrame, farmSpeed)
-                    local ModifyCFrame = TargetPart.CFrame + Vector3.new(0, -25, 0)
-                    Tp(ModifyCFrame, farmSpeed)
-                    local ModifyCFrame = TargetPart.CFrame
-                    Tp(ModifyCFrame, farmSpeed)
-                end
-
-                local CleaningParts = workspace:FindFirstChild("CleaningParts")
-                if CleaningParts then
-                    local function getPart()
-                        local playerCleanings = CleaningParts:FindFirstChildWhichIsA(Client.Name)
-                        local objPart, clickDetect;
-                        for _,part in pairs(playerCleanings:GetChildren()) do
-                            if part:FindFirstChildWhichIsA("ClickDetector") then
-                                objPart = part;
-                                clickDetect = part:FindFirstChildWhichIsA("ClickDetector")
-                            end
-                        end
-                        return objPart, clickDetect
-                    end
-
-                    local part,cd = getPart()
-                    local finish = false;
-                    Tp(part.CFrame + Vector3.new(0, 3.2, 0), farmSpeed*2)
-
-                    repeat
-                        pcall(function()
-                            PrimaryPart.Anchored = false;
-                            fireclickdetector(cd, 1)
-
-                            if not part.Parent or not part.Parent == CleaningParts then
-                                finish = true;
-                            end
-                        end)
-                    until finish
-                end
-            else -- Delivery
-                local ModifyCFrame = PrimaryPart.CFrame + Vector3.new(0, -20, 0)
-                Tp(ModifyCFrame, farmSpeed)
-                local ModifyCFrame = TargetPart.CFrame + Vector3.new(0, -25, 0)
-                Tp(ModifyCFrame, farmSpeed)
-                local ModifyCFrame = TargetPart.CFrame
-                Tp(ModifyCFrame, farmSpeed*2)
+function GetCleanParts()
+    local Folder = workspace.CleaningParts
+    local PlayerParts = Folder[Client.Name];
+    if #PlayerParts:GetChildren() > 0 then
+        local obj;
+        for i=1, #PlayerParts:GetChildren() do
+            if PlayerParts:FindFirstChild(tostring(i)) then
+                obj = PlayerParts:FindFirstChild(tostring(i))
+                break;
             end
         end
+        return obj;
     end
-    task.wait()
+    return false
 end
+
+function AutoJob(b)
+    while b do
+        if b and Client.Character then
+            pcall(function()
+                repeat
+                    task.wait()
+                until GetJob() == true
+    
+                repeat
+                    task.wait()
+                until GetLocation()
+
+                if GetLocation() then
+                    local oldTm = os.time()
+                    local Target = GetLocation()
+                    local Character = Client.Character or Client.CharacterAdded:Wait()
+                    local PrimaryPart = Character and Character:FindFirstChild("HumanoidRootPart")
+                    local Speed = Configs.farmSpeed
+                    if GetCleanParts() and PrimaryPart then
+                        local Part = GetCleanParts();
+                        local CD = Part and Part:FindFirstChildOfClass("ClickDetector")
+                        if Part and CD then
+                            Tp(Part.CFrame + Vector3.new(0, 3, 0), Speed)
+                            repeat
+                                task.wait(.5)
+                                fireclickdetector(CD, 1)
+                            until Part.Parent == nil or not workspace.CleaningParts[Client.Name]:IsAncestorOf(Part)
+                        end
+                    else
+                        if PrimaryPart and GetJob() then
+                            local ModifyCFrame = PrimaryPart.CFrame + Vector3.new(0, -20, 0)
+                            Tp(ModifyCFrame, Speed)
+
+                            local ModifyCFrame = Target.CFrame + Vector3.new(0, -25, 0)
+                            Tp(ModifyCFrame, Speed)
+
+                            local ModifyCFrame = Target.CFrame
+                            Tp(ModifyCFrame, Speed*2)
+                        end
+                    end
+
+                    task.wait(1)
+                    local curTime = os.time() - oldTm
+                    OrionLib:MakeNotification({Name = "Auto Job", Content = "Job Finished, estimated time finish: " .. tostring(curTime), Time = 5})
+                    GetJob()
+                end
+            end)
+        end
+        task.wait()
+    end
+    
+end
+
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "RXE | " .. rxe.current_game.game_name, HidePremium = false, SaveConfig = true, ConfigFolder = "RXE"})
+local Tab1 = Window:MakeTab({
+	Name = "Farm",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
+
+local SectionFarm = Tab1:AddSection({
+	Name = "Enabled"
+})
+local SectionConfigs = Tab1:AddSection({
+    Name = "Configuration"
+})
+
+SectionFarm:AddToggle({
+    Name = "Auto Job",
+    Default = false,
+    Callback = AutoJob
+})
+
+SectionConfigs:AddSlider({
+    Name = "Teleport Speed",
+    Min = 1,
+    Max = 12,
+    Default = Configs.farmSpeed,
+    Color = Color3.fromRGB(140, 0, 255),
+    Increment = 1,
+    ValueName = "MoveSpeed",
+    Callback = function(v)
+        Configs.farmSpeed = v;
+    end
+})
+
+-- while getgenv().JobFarm do
+--     local Client = Players.LocalPlayer;
+--     local Character = Client.Character or Client.CharacterAdded:Wait();
+
+--     if Character then
+--         local Events = ReplicatedStorage.Events
+--         local CoreEvent = Events.EventCore
+--         local MainGui = Client.PlayerGui:FindFirstChild("Main")
+
+--         local firedArguments = {[1] = "Job"}
+--         local Job = MainGui.LabelJob.Text
+--         local TimeJob = MainGui.LabelJob2.Text
+--         local Paycheck = MainGui.LabelJob3.Text
+--         local CancelJob = MainGui.CancelJob
+
+--         if not CancelJob.Visible then
+--             repeat task.wait()
+--                 CoreEvent:FireServer(unpack(firedArguments))
+--             until CancelJob.Visible and Client.PlayerGui:FindFirstChildWhichIsA("BillboardGui")
+--         end
+
+--         task.wait(1)
+
+--         local Billboard
+--         repeat
+--             Billboard = Client.PlayerGui:FindFirstChild("BillboardGui") or Client.PlayerGui:FindFirstChildOfClass("BillboardGui")
+--             task.wait()
+--         until Billboard
+
+--         if #Job > 0 and CancelJob.Visible and Billboard.Enabled then
+--             local TargetPart = Billboard.Adornee
+--             local PrimaryPart = Character:FindFirstChild("HumanoidRootPart")
+
+--             --: Cleaning
+--             if tostring(TargetPart):match("Entrance") then
+--                 if CLient:DistanceFromCharacter(TargetPart.Position) > 40 then
+--                     local ModifyCFrame = PrimaryPart.CFrame + Vector3.new(0, -20, 0)
+--                     Tp(ModifyCFrame, farmSpeed)
+--                     local ModifyCFrame = TargetPart.CFrame + Vector3.new(0, -25, 0)
+--                     Tp(ModifyCFrame, farmSpeed)
+--                     local ModifyCFrame = TargetPart.CFrame
+--                     Tp(ModifyCFrame, farmSpeed)
+--                 end
+
+--                 local CleaningParts = workspace:FindFirstChild("CleaningParts")
+--                 if CleaningParts then
+--                     local function getPart()
+--                         local playerCleanings = CleaningParts:FindFirstChildWhichIsA(Client.Name)
+--                         local objPart, clickDetect;
+--                         for _,part in pairs(playerCleanings:GetChildren()) do
+--                             if part:FindFirstChildWhichIsA("ClickDetector") then
+--                                 objPart = part;
+--                                 clickDetect = part:FindFirstChildWhichIsA("ClickDetector")
+--                             end
+--                         end
+--                         return objPart, clickDetect
+--                     end
+
+--                     local part,cd = getPart()
+--                     local finish = false;
+--                     Tp(part.CFrame + Vector3.new(0, 3.2, 0), farmSpeed*2)
+
+--                     repeat
+--                         pcall(function()
+--                             PrimaryPart.Anchored = false;
+--                             fireclickdetector(cd, 1)
+
+--                             if not part.Parent or not part.Parent == CleaningParts then
+--                                 finish = true;
+--                             end
+--                         end)
+--                     until finish
+--                 end
+--             else -- Delivery
+--                 local ModifyCFrame = PrimaryPart.CFrame + Vector3.new(0, -20, 0)
+--                 Tp(ModifyCFrame, farmSpeed)
+--                 local ModifyCFrame = TargetPart.CFrame + Vector3.new(0, -25, 0)
+--                 Tp(ModifyCFrame, farmSpeed)
+--                 local ModifyCFrame = TargetPart.CFrame
+--                 Tp(ModifyCFrame, farmSpeed*2)
+--             end
+--         end
+--     end
+--     task.wait()
+-- end
